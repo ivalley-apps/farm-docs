055072feb9a530ab2d6e487e45a97b4c97800539
sarathraj <srajqtr@gmail.com>
2022-02-14 19:12:31 +0300

    added external link

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+extensions = ['sphinx.ext.extlinks']
 #html_logo = u'dropwizard-logo.svg'
 
 extlinks = {
